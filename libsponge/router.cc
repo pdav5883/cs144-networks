0e7d732e0f267ce716cb8f4a35b2a53a57d30fa1
Peter Davison <davison.pl@gmail.com>
2021-12-23 13:44:23 -0500

    First draft of router

--- libsponge/router.cc
+++ libsponge/router.cc
@@ -15,9 +15,6 @@ using namespace std;
 
 // You will need to add private members to the class declaration in `router.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 //! \param[in] route_prefix The "up-to-32-bit" IPv4 address prefix to match the datagram's destination address against
 //! \param[in] prefix_length For this route to be applicable, how many high-order (most-significant) bits of the route_prefix will need to match the corresponding bits of the datagram's destination address?
 //! \param[in] next_hop The IP address of the next hop. Will be empty if the network is directly attached to the router (in which case, the next hop address should be the datagram's final destination).
@@ -29,14 +26,51 @@ void Router::add_route(const uint32_t route_prefix,
     cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/" << int(prefix_length)
          << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num << "\n";
 
-    DUMMY_CODE(route_prefix, prefix_length, next_hop, interface_num);
-    // Your code here.
+    table_item item = {route_prefix, prefix_length, next_hop, interface_num};
+    _table.push_back(item);
 }
 
 //! \param[in] dgram The datagram to be routed
 void Router::route_one_datagram(InternetDatagram &dgram) {
-    DUMMY_CODE(dgram);
-    // Your code here.
+    // drop if ttl expired or about to expire
+    if (dgram.header().ttl <= 1) {
+        return;
+    }
+
+    uint32_t dgram_dst = dgram.header().dst;
+
+    list<table_item>::iterator it = _table.begin();
+    list<table_item>::iterator longest_match = _table.end();
+    uint8_t longest_length = 0;
+
+    // look through table and find longest prefix match
+    while (it != _table.end()) {
+        uint8_t shiftnum = ADDRESS_BITS - it->prefix_length;
+        bool match = (it->route_prefix >> shiftnum) == (myaddr_numeric >> shiftnum) || shiftnum == ADDRESS_BITS;
+        if (match && it->prefix_length >= longest_length) {
+            longest_match = it;
+            longest_length = it->prefix_length;
+        }
+        it++;
+    }
+
+    // drop if we didn't find a match
+    if (longest_match == _table.end()) {
+        return;
+    }
+    // send onward if we found match
+    else {
+        Address route_dst;
+
+        if (longest_match->next_hop.has_value()) {
+            route_dst = next_hop;
+        }
+        else {
+            route_dst = Address::from_ipv4_numeric(dgram_dst);
+        }
+
+        dgram.header().ttl--;
+        interface(longest_match->interface_num).send_datagram(dgram, route_dst);
 }
 
 void Router::route() {
